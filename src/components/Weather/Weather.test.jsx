90c25045136476c793413f3f48d478d191668a47
Tulipna9 <61386138+Tulipna9@users.noreply.github.com>
2021-07-01 17:58:30 -0300

    clase 146

--- src/components/Weather/Weather.test.jsx
+++ src/components/Weather/Weather.test.jsx
@@ -6,7 +6,7 @@ import '@testing-library/jest-dom/extend-expect'
 //TDD
 test( "Weather render",async () => {
     //AAA Arrenge Act Assert
-    const { findByRole } = render(<Weather temperature={10}></Weather>)
+    const { findByRole } = render(<Weather temperature={10} state="sunny"></Weather>)
     const temp = await findByRole("heading")
 
     expect(temp).toHaveTextContent("10")
